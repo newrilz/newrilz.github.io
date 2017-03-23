519d4f29464a41c28761be759498f0037af1d5fc
newrilz <patrickrnewell2@gmail.com>
2017-03-23 09:03:48 -0600

    Update jsResumeScript.js

--- jsResumeScript.js
+++ jsResumeScript.js
@@ -1,5 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script>
      $( function() {
     $( "#accordion" ).accordion();
